ad5b894931d483f641bcd94430f648567e058610
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-08-31 23:09:23 -0700

    testing new script

--- jquery.js
+++ jquery.js
@@ -1,3 +1,23 @@
 if (jQuery){
+    var checkAnswers = function(){
+        var answerString = "";
+        var answers = $(":checked");
+        answers.each(function(i){
+            answerString = answerString + answers[i].value;
+        });
+        $(":checked").each(function(i){
+            var answerString = answerString + answers[i].value;
+        });
+        checkIfCorrect(answerString);
+    }
+
+    var checkIfCorrect = function(theString){
+        if(parseInt(theString, 16) === 81112466973){
+            $("body").addClass("correct");
+            $("hi").text("You Win!");
+            $("canvas").show();
+        }
+    }
+
     $("#question1").show();
 }
